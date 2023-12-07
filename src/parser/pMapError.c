4c6255f49c1ad50a37dbd2108feb841ea8d83fb2
Kevin Kroon <kkroon@student.codam.nl>
2023-12-07 15:21:11 +0100

    map error checking ok

--- src/parser/pMapError.c
+++ src/parser/pMapError.c
@@ -12,14 +12,46 @@
 
 #include "cub3D.h"
 
-int map_error_check(t_parser *p)
+static int	check_around(char n, char w, char s, char e)
 {
-	int i;
+	if (n == ' ' || w == ' ' || s == ' ' || e == ' ')
+		return (1);
+	return (0);
+}
 
-	i = 0;
-	while(p->map[i])
+static int	check_nearby(t_parser *p, int mlen, int y, int x)
+{
+	int	llen;
+
+	llen = int_strlen(p->map[y]);
+	if (y == 0 || y == mlen - 1 || x == 0 || x == llen - 1)
+		if (p->map[y][x] == '0')
+			return (parser_error("wrong outside\n"));
+	if (p->map[y][x] == '0')
+		if (check_around(p->map[y - 1][x], p->map[y][x - 1], p->map[y + 1][x],
+				p->map[y][x + 1]))
+			return (parser_error("wrong around\n"));
+	return (0);
+}
+
+int	map_error_check(t_parser *p)
+{
+	int	y;
+	int	x;
+	int	map_len;
+
+	map_len = get_length_arr(p->map);
+	y = 0;
+	while (p->map[y])
 	{
-		i++;
+		x = 0;
+		while (p->map[y][x])
+		{
+			if (check_nearby(p, map_len, y, x))
+				return (parser_error("map incorrection\n"));
+			x++;
+		}
+		y++;
 	}
-	return 0;
+	return (0);
 }
